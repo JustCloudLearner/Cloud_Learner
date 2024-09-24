5c5019a08ac4f3d975be6fdaf18b6485d0c13a9d
AKHAND PRATAP MISHRA <kanhamishra286@gmail.com>
2024-09-24 17:37:40 +0530

    ADDED LOGOUT API

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,19 @@
 const { loginUser } = require("../services/authService")
 
+async function logout(req,res) {
+    res.cookie('authToken' , '' , {
+        httpOnly : true ,
+        secure : false ,
+        maxAge : 10 * 24 * 60 * 60 * 1000
+    })
+    return res.status(200).json({
+        success: true,
+        message: 'SUCCESSFULY LOG OUT',
+        error: {},
+        data: {}
+    })
+}
+
 async function login(req , res) {
     try {
         
@@ -30,5 +44,6 @@ async function login(req , res) {
 }
 
 module.exports = {
-    login
+    login,
+    logout
 }
